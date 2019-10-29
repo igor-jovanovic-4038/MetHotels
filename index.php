e31a561ad4c04af270acfe110e7ad40c1e7c3a57
Igor Jovanovic <igor_jovanovic84@yahoo.com>
2019-10-29 23:05:32 +0100

    IT255-DZ01

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	$title = 'Prvi PHP projekat za domaći iz IT255';
+?>
+<!doctype html>
+<html lang="sr">
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<h1><?= $title ?></h1>
+</body>
+</html>
